2c44944fe7523c76fdaf5944955cc022713a9b55
Artem Vashchenko <ipz231_vai@student.ztu.edu.ua>
2025-09-29 22:55:04 +0300

    Added sass-embedded to the package. Added styled Welcome page

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import StartPage from "./pages/StartPage.tsx";
 import GamePage from "./pages/GamePage.tsx";
 import PlayerProvider from "./components/providers/PlayerProvider.tsx";
 import SettingsProvider from "./components/providers/SettingsProvider.tsx";
-import {Route, Routes} from "react-router-dom";
 import ResultsPage from "./pages/ResultsPage.tsx";
+import WelcomePage from "./pages/WelcomePage.tsx";
+import {Route, Routes} from "react-router-dom";
 
 function App() {
     return (
         <SettingsProvider>
             <PlayerProvider>
                 <Routes>
-                    <Route path="/"        element={<StartPage/>}/>
+                    <Route path="/"        element={<WelcomePage/>}/>
                     <Route path="/start"   element={<StartPage/>}/>
                     <Route path="/game"    element={<GamePage/>}/>
                     <Route path="/results" element={<ResultsPage/>}/>
